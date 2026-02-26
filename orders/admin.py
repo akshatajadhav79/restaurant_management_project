5a21539c35def5becab49443dcfd641e5ac7c588
akshata <akshatajadhav7902@gmail.com>
2026-02-26 20:46:38 +0530

    Add Order Status to Order Model

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Coupon,OrderStatus
+from .models import Coupon,OrderStatus,Order
 
 # Register your models here.
 admin.site.register(Coupon)
 admin.site.register(OrderStatus)
+admin.site.register(Order)
