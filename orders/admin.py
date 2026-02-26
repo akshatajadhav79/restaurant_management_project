d05a24fe1a4f1aa6bb212ad281103c7487b3b4fa
akshata <akshatajadhav7902@gmail.com>
2026-02-26 20:26:44 +0530

    Create a Simple Order Status Model

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Coupon
+from .models import Coupon,OrderStatus
 
 # Register your models here.
 admin.site.register(Coupon)
+admin.site.register(OrderStatus)
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,4 @@
+from os import name
 from pyexpat import model
 
 from django.db import models
@@ -12,3 +13,9 @@ class Coupon(models.Model):
     
     def __str__(self):
         return f'{self.code} - {self.discount_percentage}%'
+    
+class OrderStatus(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.name
