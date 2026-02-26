bc176a88945959d22b0f596aab2370f124a9b066
akshata <akshatajadhav7902@gmail.com>
2026-02-26 18:40:00 +0530

    Create a Coupon Model and API Endpoint for Validation

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,48 @@
+import dis
+from email import utils
+
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.utils import timezone
+from .models import Coupon
 
 # Create your views here.
+
+class CouponValidationView(APIView):
+    def post(self,request):
+        code = request.data.get('code')
+        
+        if not code:
+            return Response(
+                {"error":"Coupon code is required"},
+                status = status.HTTP_400_BAD_REQUEST
+            )
+        
+        coupon = Coupon.objects.filter(code__iexact=code).first()
+        if not coupon:
+            return Response(
+                {"error": "Invalid coupon code."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        today = timezone.now().date()
+        
+        if not coupon.is_active:
+            return Response(
+                {"error":"Coupon is inactive"},
+                status = status.HTTP_400_BAD_REQUEST
+            )
+            
+        if not (coupon.valid_from <= today <= coupon.valid_until):
+            return Response(
+                {"error":"coupon is expired or not yet valid"},
+                status = status.HTTP_400_BAD_REQUEST
+            )
+        
+        return Response(
+            {"message":"Coupon is valid",
+             "discount_percentege":coupon.discount_percentage },
+            status = status.HTTP_200_OK
+        )
