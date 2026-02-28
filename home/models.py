b53dabad990a1763ddfe42d85dbe65bde3365e4e
Akshata <141274945+akshatajadhav79@users.noreply.github.com>
2026-02-28 20:31:41 +0530

    66808

--- home/models.py
+++ home/models.py
@@ -15,5 +15,5 @@ class MenuItem(models.Model):
     price = models.DecimalField(max_digits=8,decimal_places=2)
     is_featured = models.BooleanField(default=False)
     
-    def __str__(self):
+    def __str__(self): 
         return self.name
