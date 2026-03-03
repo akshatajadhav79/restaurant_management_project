2e6100741abf1901f6ba315874059b4c597b594a
Akshata <141274945+akshatajadhav79@users.noreply.github.com>
2026-03-03 23:34:22 +0530

    66872

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from .models import MenuCategory,MenuItem
 from .serializers import MenuCategorySerializer,MenuItemSerializer
 from rest_framework.generics import ListAPIView
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 
 # Create your views here.
 
@@ -14,3 +15,19 @@ class FeaturedMenuItemListView(generics.ListAPIView):
     def get_queryset(self):
         return MenuItem.objects.filter(is_featured = True)
     
+class MenuItemPagiantion(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 50
+
+class MenuItemViewSet(ModelViewSet):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    pagination_class = MenuItemPagiantion
+
+    def get_queryset(self):
+        queryset = MenuItem.objects.all()
+        search_query = self.request.query_params.get('search')
+        if search_query:
+            queryset = queryset.filter(name__icontains = search_query)
+        return queryset
