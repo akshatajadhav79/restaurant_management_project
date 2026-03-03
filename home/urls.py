ed78584a1730406c48a1ec3ba0016e3d2d73de21
Akshata <141274945+akshatajadhav79@users.noreply.github.com>
2026-03-03 23:46:19 +0530

    66872

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('categories/' ,MenuCategoryListView.as_view(),name = 'menu-category-list') ,
     path('menu/featured/',FeaturedMenuItemListView.as_view(),name = 'featured-menu'), 
+    path('menu-items',)
 
 ]
