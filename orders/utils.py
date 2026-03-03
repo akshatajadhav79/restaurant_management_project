bcc1e5474670b7d7fb52bca99a7c60f6a04b33ba
Akshata <141274945+akshatajadhav79@users.noreply.github.com>
2026-03-04 00:00:28 +0530

    66871

--- orders/utils.py
+++ orders/utils.py
@@ -1,12 +1,19 @@
 import string 
 import secrets
+from decimal Decimal,ROUND_HALF_UP 
 # from django.apps import apps
 
 def generate_coupon_code(length=10):
     characters = string.ascii_uppercase + string.digits
     
-    while True:
+    while True: 
         code = ''.join(secrets.choice(characters) for _ in range(length)) 
         if not Coupon.objects.filter(code = code).exists():
             return code
+
+def calculate_tip_amount(order_total,tip_precentage):
+    order_total  = Decimal(str(order_total))
+    tip_precentage = Decimal(str(tip_precentage))
+    tip_amount = order_total * (tip_precentage/Decimal("100"))
+    return tip_amount.quantize(Decimal("0.01"),rounding = ROUND_HALF_UP)
           
