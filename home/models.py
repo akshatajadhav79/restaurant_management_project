4b78b06b1295d63d8c8f2c0af583df9ec03aaaa3
Akshata <141274945+akshatajadhav79@users.noreply.github.com>
2026-03-06 23:54:52 +0530

    66917

--- home/models.py
+++ home/models.py
@@ -1,5 +1,3 @@
-from pyexpat import model
-
 from django.db import models
 
 # Create your models here.
@@ -8,13 +6,19 @@ class MenuCategory(models.Model):
     
     def __str__(self):
         return self.name  
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
     
 class MenuItem(models.Model):
     name = models.CharField(max_length=250)
     description = models.TextField(blank = True)   
     price = models.DecimalField(max_digits=8,decimal_places=2)
     is_featured = models.BooleanField(default=False)
-    
+    ingredients = models.ManyToManyField(Ingredient,related_name = 'menu_items')
+
     def __str__(self): 
         return self.name   
 
