e7c46883c188018bb5c1d5ad93ba625a9b4af875
Akshata <141274945+akshatajadhav79@users.noreply.github.com>
2026-03-07 00:14:22 +0530

    66917

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('categories/' ,MenuCategoryListView.as_view(),name = 'menu-category-list') ,
     path('menu/featured/',FeaturedMenuItemListView.as_view(),name = 'featured-menu'), 
-    path('menu-items',MenuItemViewSet.as_view(),name = 'menu-items')
+    path('menu-items',MenuItemViewSet.as_view(),name = 'menu-items'),
+    path("api/menu-items/<int:pk>/ingredients/",MenuItemIngredientsView.as_view(),name='menu-item-ingredients')
 
 ]
