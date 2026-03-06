a69393111bdb61694584782907c818f2d5860cfb
Akshata <141274945+akshatajadhav79@users.noreply.github.com>
2026-03-06 17:18:38 +0530

    66919

--- home/models.py
+++ home/models.py
@@ -30,12 +30,19 @@ class NutritionalInformation(models.Model):
         return f"{self.menu_item.name} - {self.calories} Kcal"
 
 
+class DailySpecialManager(models.Manager):
+    def upcoming (self):
+        today = datetime.date.today()
+        return self.filter(date__gte = today)
+
 class DailySpecial(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=8,decimal_places=2)
     is_activate = models.BooleanField(default=True)
     create_at = models.DateTimeField(auto_now_add = True)
+    objects = DailySpecialManager()
+
     def __str__(self):
         return self.title
 
