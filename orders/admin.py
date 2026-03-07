a5c2c713c2a2e0bd0fbbc794db1d1034276bd040
Akshata <141274945+akshatajadhav79@users.noreply.github.com>
2026-03-07 14:38:50 +0530

    67004

--- orders/admin.py
+++ orders/admin.py
@@ -5,3 +5,14 @@ from .models import Coupon,OrderStatus,Order
 admin.site.register(Coupon)
 admin.site.register(OrderStatus)
 admin.site.register(Order)
+
+def mark_orders_processed(modeladmin,request,queryset):
+    queryset.update(status = 'Processed')
+
+mark_orders_processed.short_description = "Mark selected orders as Processed"
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id','customer','status','created_at')
+    actions = [mark_orders_processed]
+
+admin.site.register(Order,OrderAdmin)
