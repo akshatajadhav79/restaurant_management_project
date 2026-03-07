8427eb82968ffc5a63923f1663de11ec9869cc18
Akshata <141274945+akshatajadhav79@users.noreply.github.com>
2026-03-07 14:25:23 +0530

    67005

--- account/views.py
+++ account/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from utils.validation_utils import validate_email_address
+from rest_framework.response import response
 # Create your views here.
+
+def resgister_user(request):
+    email = request.data.get("email")
+
+    if not validate_email_address(email):
+        return response({"error":"Invalid email address"},status = 400)
