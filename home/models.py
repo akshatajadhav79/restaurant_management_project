c85692499ecb484991163d4312c1a4d0d098069e
Akshata <141274945+akshatajadhav79@users.noreply.github.com>
2026-03-09 16:41:36 +0530

    67002

--- home/models.py
+++ home/models.py
@@ -57,3 +57,9 @@ class DailySpecial(models.Model):
             return specials.order_by('?').first()
         return None
 
+class Table(models.Model):
+    table_number = models.IntergerField()
+    capacity = models.IntergerField()
+    is_available =models.BooleanField(default=True)
+    def __str__(self):
+        return f"Table {self.table_number}"
