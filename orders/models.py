afde1be8a436116aba37f0bea00475460c26d4ec
Akshata <141274945+akshatajadhav79@users.noreply.github.com>
2026-03-11 17:46:28 +0530

    67074

--- orders/models.py
+++ orders/models.py
@@ -1,9 +1,5 @@
-from pyexpat import model
-
 from django.db import models
 from orders.utils import generate_coupon_code
-from django.contrib.auth.models import User
-
 
 # Create your models here.
 class Coupon(models.Model):
@@ -26,24 +22,24 @@ class OrderStatus(models.Model):
     
     def __str__(self): 
         return self.name
+
+class OrderManager(models.Manager):
+    def get_active_orders(self):
+        return self.filter(status__in =['pending','processing'])
     
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='orders')
+    STATUS_CHOICES = [
+        ('pending','Pending'),('processing','Processsing'),
+        ('completed','Completed'),('canceled','Canceled')
+    ]
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    total_price = models.DecimalField(max_digits=10,decimal_places=2,default=0.00)
-    status = models.ForeignKey(OrderStatus,
+    Ostatus = models.ForeignKey(OrderStatus,
         on_delete=models.SET_NULL,
         null=True,
         blank=True
     )
+    status = models.CharField(max_length = 20,choices = STATUS_CHOICES)
+    object = OrderManager()
     def __str__(self):
         return f"Order {self.id}"
-    
-class OrderItem(models.Model):
-    order = models.ForeignKey(Order,on_delete=models.CASCADE,related_name='items')
-    menu_item_name = models.CharField(max_length=255)
-    quantity = models.PositiveIntegerField()
-    price = models.DecimalField(max_digits=8,decimal_places=2)
-    
-    def __str__(self):
-        return self.menu_item_name
