ed2163904d238e42b6d4ad0c23f2f7f05415cc81
Akshata <141274945+akshatajadhav79@users.noreply.github.com>
2026-03-27 20:51:51 +0530

    68418

--- orders/urls.py
+++ orders/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('coupons/validate/' ,CouponValidationView.as_view(),name='coupon-validate'),
     path('history/',OrderHistoryView.as_view(),name = 'order-history'),
+    path('orders/<int:pk>/update-status/',UpdateOrderStatusView.as_view(),name = 'update-order-status'),
 
 ]
