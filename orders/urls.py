c5c559df0533736c2c839f9552cf1fe748050b89
Akshata <141274945+akshatajadhav79@users.noreply.github.com>
2026-03-29 17:05:48 +0530

    68419

--- orders/urls.py
+++ orders/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('coupons/validate/' ,CouponValidationView.as_view(),name='coupon-validate'),
     path('history/',OrderHistoryView.as_view(),name = 'order-history'),
     path('orders/<int:pk>/update-status/',UpdateOrderStatusView.as_view(),name = 'update-order-status'),
+    path('payment-methods/',PaymentMethodListAPIView.as_view(),name = 'payment-methods'),
 
 ]
