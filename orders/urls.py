8c4596d2c60f107db121f834c61efe51a4d07fd4
Akshata <141274945+akshatajadhav79@users.noreply.github.com>
2026-03-29 17:28:08 +0530

    68420

--- orders/urls.py
+++ orders/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 from .views import *
-router = DefaultRouter()
-router.register(r'orders',orderViewSet,basename = 'orders')
-url0
 
 urlpatterns = [
     path('coupons/validate/' ,CouponValidationView.as_view(),name='coupon-validate'),
     path('history/',OrderHistoryView.as_view(),name = 'order-history'),
     path('orders/<int:pk>/update-status/',UpdateOrderStatusView.as_view(),name = 'update-order-status'),
     path('payment-methods/',PaymentMethodListAPIView.as_view(),name = 'payment-methods'),
-
+    path('orders/<int:pk>/cancel/',CancelOrderView.as_view())
 
 ]
