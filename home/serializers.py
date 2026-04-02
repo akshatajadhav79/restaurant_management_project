b8ea0c6223b6e90e0c801d42cb6ab6205acac7f0
Akshata <141274945+akshatajadhav79@users.noreply.github.com>
2026-04-02 17:38:28 +0530

    68782

--- home/serializers.py
+++ home/serializers.py
@@ -1,8 +1,6 @@
-from dataclasses import field
-from pyexpat import model
 
 from rest_framework import serializers
-from .models import MenuCategory,MenuItem,Ingredient,Table
+from .models import MenuCategory,MenuItem,Ingredient,Table,ContactFormSubmission
 
 class MenuCategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -25,4 +23,16 @@ class TableSerializer(serializers.ModelSerializer):
         model = Table
         fields = "__all__"
     
+class ContactFormSubmissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContactFormSubmission
+        fields=['id','name','email','message','created_at']
+        def validation_name(self,val):
+            if len(val)<2:
+                raise serializers.ValidationError("Name must be at least 2 characterlong.")
+            return value
 
+class DailySpecialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItem
+        fields = ['id','name','description','price']
