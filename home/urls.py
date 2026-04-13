98fd311f13686c95e5acee03e6f34b4a314c186b
Akshata <141274945+akshatajadhav79@users.noreply.github.com>
2026-04-13 23:22:42 +0530

    70207

--- home/urls.py
+++ home/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("restaurant/",RestaurantDetailView.as_view(),name = 'restaurant-detail'),
     path("restaurant/,<int:id>/",RestaurantDetailView1.as_view(),name = 'restaurant-detail-view'),
     path("menu/search/",MenuItemSearchView.as_view(),name = 'menu-search'),
+    path('restaurant/opening-hours/',OpeningHoursView.as_view(),name = "opening-hours")
 ]
